def56d581d4f9a4a7f9aed1713ec6b23df07457d
Marcos Gomes <marcosgomesdev@gmail.com>
2024-04-04 16:16:55 -0300

    feat: create get event attendees router

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import { checkIn } from "./routes/check-in";
 import { createEvent } from "./routes/create-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { getEvent } from "./routes/get-event";
+import { getEventAttendees } from "./routes/get-event-attendees";
 import { registerForEvent } from "./routes/register-for-event";
 
 const app = fastify();
@@ -19,6 +20,7 @@ app.register(registerForEvent);
 app.register(getEvent);
 app.register(getAttendeeBadge);
 app.register(checkIn);
+app.register(getEventAttendees);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("http server runing");
